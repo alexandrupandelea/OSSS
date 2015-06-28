a97a6b0aa01fd4fc22be8641153e2fec1d46207f
Pandelea Alexandru <alexandru.pandelea@gmail.com>
2015-06-28 12:12:09 +0300

    Added blank template page

--- django_project/movierater/views.py
+++ django_project/movierater/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def movie_list(request):
+	return render(request, 'movierater/movie_list.html', {})
--- django_project/moviesite/urls.py
+++ django_project/moviesite/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'moviesite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'', include('movierater.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+#url(r'^$', views.post_list)
