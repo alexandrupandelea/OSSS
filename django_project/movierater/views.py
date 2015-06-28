60d8a55a7c32184b458433874dde20b5156d14ff
Pandelea Alexandru <alexandru.pandelea@gmail.com>
2015-06-28 13:49:11 +0300

    added views and beta html page

--- django_project/movierater/views.py
+++ django_project/movierater/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Movie
+from .models import Rating
 
 def movie_list(request):
-	return render(request, 'movierater/movie_list.html', {})
+	movies = Movie.objects.order_by('title')
+	return render(request, 'movierater/movie_list.html', {'movies': movies})
